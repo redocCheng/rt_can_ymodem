e23d260390f803102552326ce795ea3a6de4ff1c
redoc <619675912@qq.com>
2021-01-04 10:18:38 +0800

    Update drv_can_ymodem.h

--- demo/drv_can_ymodem.h
+++ demo/drv_can_ymodem.h
@@ -1,1 +1,18 @@
-void drv_can_ymodem_recv(const void  *buffer, rt_size_t size);
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020-04-29     redoc         first version.
+ */
+
+#ifndef __DRV_CAN_YMODEM_H__
+#define __DRV_CAN_YMODEM_H__
+
+#include <rtthread.h>
+
+void drv_can_ymodem_recv(uint32_t ch, const void *buffer, rt_size_t size);
+
+#endif
